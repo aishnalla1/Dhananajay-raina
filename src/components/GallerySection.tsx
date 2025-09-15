055f2b0ef6ddabc59fb9bdabaf5d05a4221f4e55
aishnalla1 <nallaaishwarya24@gmail.com>
2025-09-15 19:48:12 +0530

    updated video

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -19,7 +19,7 @@ const DhahanPerformanceSection = () => {
           {/* Video */}
           <div className="flex items-center justify-center">
             <video
-              src="/videos/singing.mp4"
+              src="/videos/sing.mp4"
               controls
               className="w-full h-auto rounded-lg shadow-md"
             />
